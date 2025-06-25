786caa53c2b0d358b3d474921176c699c85ccb91
Milica Simic <milica.simic1999@gmail.com>
2025-06-25 16:32:20 +0200

    inline stilizacija

--- src/components/OneProduct.tsx
+++ src/components/OneProduct.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const OneProduct = () => {
+    const stil = {
+        margin: 10,
+        borderStyle: 'dashed',
+    }
   return (
-    <div className="card">
+    // border-style; margin-top
+    <div className="card" style={stil}>
         <img className="card-img-top" src="https://picsum.photos/200" alt='Product image'/>
         <div className="card-body">
             <h3 className="card-title">Product name</h3>
