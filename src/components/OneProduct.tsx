f4e6c577ebcdb9e34f4a6b0ef266d3bc818ec899
Milica Simic <milica.simic1999@gmail.com>
2025-06-28 15:38:13 +0200

    props

--- src/components/OneProduct.tsx
+++ src/components/OneProduct.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Product } from '../models/product'
 
-const OneProduct = () => {
+interface OneProductProps {
+  productProps: Product;
+  // amount: number;
+}
+
+
+const OneProduct : React.FC<OneProductProps> = ({productProps}) => {
     const stil = {
         margin: 10,
         borderStyle: 'dashed',
@@ -10,8 +17,9 @@ const OneProduct = () => {
     <div className="card" style={stil}>
         <img className="card-img-top" src="https://picsum.photos/200" alt='Product image'/>
         <div className="card-body">
-            <h3 className="card-title">Product name</h3>
-            <p className="card-text">Product description....</p>
+            {/* <h3 className="card-title">{props.productProps.name}</h3> */}
+            <h3 className="card-title">{productProps.name}</h3>
+            <p className="card-text">{productProps.description}</p>
             <a href="" className='btn'>+</a>
             <a href="" className='btn'>-</a>
         </div>
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import OneProduct from './OneProduct'
+import { Product } from '../models/product'
 
+const product1 = new Product(1, 'Proizvod 1', 'Opis proizvoda 1',0);
 const Products = () => {
   return (
     <div className='all-products'>
-        <OneProduct />
-        <OneProduct />
-        <OneProduct />
-        <OneProduct />
+      {/* CTRL + ALT + strelica na dole/gore */}
+        <OneProduct productProps={product1}/>
+        <OneProduct productProps={product1}/>
+        <OneProduct productProps={product1}/>
+        <OneProduct productProps={product1}/>
     </div>
   )
 }
