837045436dc4baaa057689d65aabef130040ec4d
Milica Simic <milica.simic1999@gmail.com>
2025-06-28 15:55:39 +0200

    podizanje stanja

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Products from './components/Products';
+import { Product } from './models/product';
+
+const products: Product[] = [
+  new Product(1, 'Proizvod 1', 'Opis proizvoda 1', 0),
+  new Product(2, 'Proizvod 2', 'Opis proizvoda 2', 0),
+  new Product(3, 'Proizvod 3', 'Opis proizvoda 3', 0),
+  new Product(4, 'Proizvod 4', 'Opis proizvoda 4', 0),
+];
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Products />
+      <Products productsProps={products}/>
     </div>
   );
 }
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import OneProduct from './OneProduct'
 import { Product } from '../models/product'
 
+interface ProductsProps {
+  productsProps: Product[];
+}
 const product1 = new Product(1, 'Proizvod 1', 'Opis proizvoda 1',0);
-const Products = () => {
+const Products : React.FC<ProductsProps> = ({productsProps}) => {
   return (
     <div className='all-products'>
       {/* CTRL + ALT + strelica na dole/gore */}
-        <OneProduct productProps={product1}/>
-        <OneProduct productProps={product1}/>
-        <OneProduct productProps={product1}/>
-        <OneProduct productProps={product1}/>
+        <OneProduct productProps={productsProps[0]}/>
+        <OneProduct productProps={productsProps[1]}/>
+        <OneProduct productProps={productsProps[2]}/>
     </div>
   )
 }
